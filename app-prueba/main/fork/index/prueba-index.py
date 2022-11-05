86ecf5b997d7e7b3b5507a11740f25a48cce6a0a
natyr55 <nvrincon2@gmail.com>
2022-11-05 11:36:16 -0300

    Create prueba-index.py

---
+++ app-prueba/main/fork/index/prueba-index.py
@@ -0,0 +1,65 @@
+
+#*********************************
+# Primer commit main app-prueba
+# subir a Github
+# index
+# ejecutable.exe
+# release: 26/10/2022
+# Developer: Natalia Rincon
+#**********************************
+
+
+# main - resources
+Marca = "samsung" , "motorola", "iphone", "xioami"
+
+Modelo = "GALAXI A03" ,  "GALAXI A13" ,"GALAXI A33" , "GALAXI A22", "GALAXI A52","GALAXI A32" , "GALAXI A53", \
+"GALAXI S22 PLUS",  "GALAXI S22 ULTRA", "MOTOROLA MOTO G71", "MOTOROLA MOTO G22", "MOTOROLA MOTO G52" , \
+"MOTOROLA EDGE 30", "MOTOROLA FUSION 5G", "MOTOROLA MOTO E20", "MOTOROLA MOTO E40", "MOTOROLA MOTO G41", \
+"MOTOROLA MOTO G51 5G", "MOTOROLA MOTO G42", "MOTOROLA MOTO G82 5G", "MOTOROLA EDGE 30 5G" ,  "MOTOROLA EDGE 20 PRO 5G", \
+"MOTOROLA EDGE 30 PRO REMOTE", "Motorola edge 30 pro STYLUS EDITION", "IPHONE 13", "IPHONE 13 PRO", "IPHONE 13 PRO MAX",  \
+"IPHONE 11", "IPHONE 11 MINI" , "IPHONE 12","IPHONE 12 PRO", "IPHONE 12 PRO MAX" , "Redmi Note 10 5G 128GB Graphite Gray", \
+"Redmi Note 11S Twilight Blue 6GB RAM 128GB ROM" , "Xiaomi Redmi 10 6GB 128GB", "Xiaomi Redmi 10 4GB 128GB" , \
+"Xiaomi Poco X4 Pro 6GB 128GB Yellow Sin Cargador"
+
+Cantidad = "CANT1 DE 1 A 5 UNIDADES" , "CANT2 DE 5 A 10 UNIDADES" , "CANT3 DE 10 A 15 UNIDADES" , "CANT4 MAS DE 15 UNIDADES"
+
+Precio = " RAN1 DE $30.000 A $45.000" , "RAN2 DE $45.000 A $60.000" , "RANGO3 MAS DE $60.000"
+
+Color = "NEGRO" , "AZUL" , "GRIS" , "ROJO" , "DORADO"
+
+Zona = 1, 2, 3 
+
+#index
+
+
+
+
+
+Marca = input("selecciona una marca")
+print(" usted selecciono," + Marca)
+
+Modelo = input("selecciona un modelo")
+print(" usted selecciono, " + Modelo)
+
+Cantidad = input("selecciona la cantidad")
+print("usted selecciono " + Cantidad)
+
+Precio = input("selecciona el precio")
+print("Usted selecciono" + Precio)
+
+Color = input("selecciono color")
+print("Usted selecciono color" + Color)
+
+Zona = input("seleccione zona")
+print("Usted selecciono" + Zona)
+
+
+
+
+
+
+
+
+
+
+
